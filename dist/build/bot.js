26d1ccd3e7ea2693f77a9dae7411ae98f8991bf7
GoierBachus1 <etza123les@gmail.com>
2023-04-25 11:49:05 -0600

    Second change

--- dist/build/bot.js
+++ dist/build/bot.js
@@ -12,45 +12,37 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const telegraf_1 = require("telegraf");
 const messages_1 = require("./constants/messages");
 const bot = new telegraf_1.Telegraf(process.env.TOKEN || '');
-bot.start((ctx) => __awaiter(void 0, void 0, void 0, function* () {
-    let numMembers = yield ctx.getChatMembersCount();
-    if (numMembers > 2)
-        return ctx.deleteMessage(ctx.message.message_id);
-    return ctx.replyWithHTML(messages_1.initialMessage);
-}));
-
 class Grupo {
     constructor(nombre) {
         this.nombre = nombre;
         this.integrantes = [];
         this.tareas = [];
     }
-  
-    agregarUsuario(usuario) {
-      this.usuarios.push(usuario);
+    agregarIntegrante(integrante) {
+        this.integrantes.push(integrante);
     }
-  
-    getUsuarios() {
-      return this.usuarios;
+    obtenerIntegrantes() {
+        return this.integrantes;
     }
     dividirEnSalas(numSalas) {
         const salas = [];
-        
         const integrantesPorSala = Math.ceil(this.integrantes.length / numSalas);
         for (let i = 0; i < numSalas; i++) {
-          const inicio = i * integrantesPorSala;
-          const fin = inicio + integrantesPorSala;
-          const sala = this.integrantes.slice(inicio, fin);
-          salas.push(`Sala ${i + 1}: ${sala.join(", ")}`);
+            const inicio = i * integrantesPorSala;
+            const fin = inicio + integrantesPorSala;
+            const sala = this.integrantes.slice(inicio, fin);
+            salas.push(`Sala ${i + 1}: ${sala.join(", ")}`);
         }
-    
         return salas;
-      }
-  }
-  const grupos = {};
-
- 
-  
+    }
+}
+const grupos = {};
+bot.start((ctx) => __awaiter(void 0, void 0, void 0, function* () {
+    let numMembers = yield ctx.getChatMembersCount();
+    if (numMembers > 2)
+        return ctx.deleteMessage(ctx.message.message_id);
+    return ctx.replyWithHTML(messages_1.initialMessage);
+}));
 bot.command('myteam', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
     let numMembers = yield ctx.getChatMembersCount();
     if (numMembers == 2)
@@ -70,62 +62,16 @@ bot.command('/members', (ctx) => __awaiter(void 0, void 0, void 0, function* () 
         return ctx.deleteMessage(ctx.message.message_id);
     return ctx.reply('Members:\n');
 }));
-
-bot.command('crear_grupo', async (ctx) => {
+bot.command('crear_grupo', (ctx) => __awaiter(void 0, void 0, void 0, function* () {
     const chatId = ctx.chat.id;
     const nombreGrupo = ctx.message.text.split(' ')[1];
     if (!nombreGrupo) {
-      return ctx.reply('Debes especificar un nombre para el grupo.');
+        return ctx.reply('Debes especificar un nombre para el grupo.');
     }
     if (grupos[nombreGrupo]) {
-      return ctx.reply(`Ya existe un grupo con el nombre ${nombreGrupo}.`);
+        return ctx.reply(`Ya existe un grupo con el nombre ${nombreGrupo}.`);
     }
     grupos[nombreGrupo] = new Grupo(nombreGrupo);
     return ctx.reply(`El grupo ${nombreGrupo} ha sido creado correctamente.`);
-  });
-  bot.command('unir_grupo', async (ctx) => {
-    const chatId = ctx.chat.id;
-    const nombreGrupo = ctx.message.text.split(' ')[1];
-    if (!nombreGrupo) {
-      return ctx.reply('Debes especificar un nombre para el grupo.');
-    }
-    const grupo = grupos[nombreGrupo];
-    if (!grupo) {
-      return ctx.reply(`El grupo ${nombreGrupo} no existe.`);
-    }
-    grupo.agregarUsuario(chatId);
-    return ctx.reply(`Te has unido al grupo ${nombreGrupo}.`);
-  });
-  
-  bot.command('ver_grupos', async (ctx) => {
-    let mensaje = '';
-    for (const nombreGrupo in grupos) {
-      const grupo = grupos[nombreGrupo];
-      const usuarios = grupo.getUsuarios().length;
-      mensaje += `Grupo: ${nombreGrupo}, Usuarios: ${usuarios}\n`;
-    }
-    return ctx.reply(mensaje || 'No hay grupos creados.');
-  });
-
-  bot.command('dividir_grupo', async (ctx) => {
-    const chatId = ctx.chat.id;
-    const nombreGrupo = ctx.message.text.split(' ')[1];
-    const numSalas = ctx.message.text.split(' ')[2];
-    if (!nombreGrupo || !numSalas) {
-      return ctx.reply('Debes especificar el nombre del grupo y el número de salas en las que quieres dividirlo.');
-    }
-    if (!grupos[nombreGrupo]) {
-      return ctx.reply(`No existe un grupo con el nombre ${nombreGrupo}.`);
-    }
-    if (isNaN(numSalas) || numSalas < 1) {
-      return ctx.reply('El número de salas debe ser un número entero positivo.');
-    }
-    const salas = grupos[nombreGrupo].dividirEnSalas(numSalas);
-    let respuesta = `El grupo ${nombreGrupo} ha sido dividido en ${numSalas} salas con los siguientes nombres:\n\n`;
-    salas.forEach((sala, index) => {
-    respuesta += `Sala ${index + 1}: ${sala}\n`;
-    });
-    return ctx.reply(respuesta);
-  });
-  
+}));
 exports.default = bot;
--- src/bot.ts
+++ src/bot.ts
@@ -3,6 +3,40 @@ import { initialMessage, teamAddedMessage } from './constants/messages'
 
 
 const bot = new Telegraf(process.env.TOKEN || '')
+class Grupo {
+    nombre: string;
+    integrantes: string[];
+    tareas: string[];
+
+    constructor(nombre: string) {
+        this.nombre = nombre;
+        this.integrantes = [];
+        this.tareas = [];
+    }
+
+    agregarIntegrante(integrante: string) {
+        this.integrantes.push(integrante);
+    }
+
+    obtenerIntegrantes() {
+        return this.integrantes;
+    }
+
+    dividirEnSalas(numSalas: number) {
+        const salas: string[] = [];
+        const integrantesPorSala = Math.ceil(this.integrantes.length / numSalas);
+
+        for (let i = 0; i < numSalas; i++) {
+            const inicio = i * integrantesPorSala;
+            const fin = inicio + integrantesPorSala;
+            const sala = this.integrantes.slice(inicio, fin);
+            salas.push(`Sala ${i + 1}: ${sala.join(", ")}`);
+        }
+
+        return salas;
+    }
+}
+const grupos: {[nombreGrupo: string]: Grupo} = {};
 
 bot.start(async ctx => {
 	let numMembers = await ctx.getChatMembersCount()
@@ -37,6 +71,19 @@ bot.command('/members', async ctx => {
 
 	return ctx.reply('Members:\n')
 })
+bot.command('crear_grupo', async (ctx) => {
+	const chatId: number = ctx.chat.id;
+	const nombreGrupo: string = ctx.message.text.split(' ')[1];
+	if (!nombreGrupo) {
+	  return ctx.reply('Debes especificar un nombre para el grupo.');
+	}
+	if (grupos[nombreGrupo]) {
+	  return ctx.reply(`Ya existe un grupo con el nombre ${nombreGrupo}.`);
+	}
+	grupos[nombreGrupo] = new Grupo(nombreGrupo);
+	return ctx.reply(`El grupo ${nombreGrupo} ha sido creado correctamente.`);
+  });
+
 
 
 export default bot
